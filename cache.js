37ebd31bd5d2c2a14f1024d638399654e584d40e
kstost <monogatree@gmail.com>
2020-03-14 17:41:07 +0900

    fixed

---
+++ cache.js
@@ -0,0 +1,21 @@
+(() => {
+	    let elements = {};
+	    elements.id = {};
+	    elements.class = {};
+	    elements.tagName = {};
+	    for (let element of document.querySelectorAll('*')) {
+		            if (element.id) {
+				                elements.id[element.id] = element;
+				            }
+		            if (element.className && typeof element.className === 'string') {
+				                element.className.split(' ').forEach(function (class_) {
+							                if (!elements.class[class_]) { elements.class[class_] = []; }
+							                elements.class[class_].push(element);
+							            });
+				            }
+		            let tagName = element.tagName.toLowerCase();
+		            if (!elements.tagName[tagName]) { elements.tagName[tagName] = []; }
+		            elements.tagName[tagName].push(element);
+		        }
+	    return elements;
+})();
